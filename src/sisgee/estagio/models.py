76ad03d0e4c3267ec5a74c684b77574181f685e0
Lehnemann <lehneman@yahoo.com.br>
2017-11-12 01:38:51 -0200

    Alterações no modelo e importação csv
    Finalizei a alterações necessárias para o funcionamento da função de importação do arquivo .csv. Incluindo o log dos registros com erro.

--- src/sisgee/estagio/models.py
+++ src/sisgee/estagio/models.py
@@ -4,32 +4,29 @@ from .validators import validate_cpf
 
 
 class Pessoa(models.Model):
-    cpf = models.CharField(db_column='opf', unique=True, max_length=11, validators=[validate_cpf])
+    cpf = models.CharField(db_column='cpf', unique=True, max_length=14, validators=[validate_cpf])
     nome = models.CharField(db_column='nome', max_length=100)
     dt_nasc = models.DateField(db_column='dataNascimento')
-    tipo_endereco = models.CharField(db_column='tipoEndereco', max_length=100)
-    endereco = models.CharField(db_column='endereco', max_length=255)
-    numero = models.CharField(db_column='numeroEndereco', max_length=10)
-    complemento = models.CharField(db_column='complementoEndereco', max_length=150)
-    bairro = models.CharField(db_column='bairroEndereco', max_length=150)
-    cep = models.CharField(db_column='cepEndereco', max_length=15)
-    # distrito = models.CharField(db_column='distritoEndereco', max_length=150, blank=True, null=True)
-    cidade = models.CharField(db_column='cidadeEndereco', max_length=150)
-    estado = models.CharField(db_column='estadoEndereco', max_length=2)
-    pais = models.CharField(db_column='paisEndereco', max_length=100)
+    tipo_endereco = models.CharField(db_column='tipoEndereco', max_length=100, blank=True, null=True)
+    endereco = models.CharField(db_column='endereco', max_length=255, blank=True, null=True)
+    numero = models.CharField(db_column='numeroEndereco', max_length=10, blank=True, null=True)
+    complemento = models.CharField(db_column='complementoEndereco', max_length=150, blank=True, null=True)
+    bairro = models.CharField(db_column='bairroEndereco', max_length=150, blank=True, null=True)
+    cep = models.CharField(db_column='cepEndereco', max_length=15, blank=True, null=True)
+    distrito = models.CharField(db_column='distritoEndereco', max_length=150, blank=True, null=True)
+    cidade = models.CharField(db_column='cidadeEndereco', max_length=150, blank=True, null=True)
+    estado = models.CharField(db_column='estadoEndereco', max_length=2, blank=True, null=True)
+    pais = models.CharField(db_column='paisEndereco', max_length=100, blank=True, null=True)
     email = models.EmailField(db_column='email', max_length=150, blank=True, null=True)
-    # ddi_residencial = models.IntegerField(db_column='Ddi_Residencial', blank=True, null=True)
-    # ddd_residencial = models.IntegerField(db_column='Ddd_Residencial', blank=True, null=True)
-    fone_residencial01 = models.IntegerField(db_column='telefoneResidencial01')
-    fone_residencial02 = models.IntegerField(db_column='telefoneResidencial02', blank=True, null=True)
-    # ddi_comercial = models.IntegerField(db_column='Ddi_Comercial', blank=True, null=True)
-    # ddd_comercial = models.IntegerField(db_column='Ddd_Comercial', blank=True, null=True)
-    fone_comercial01 = models.IntegerField(db_column='telefoneComercial01', blank=True, null=True)
-    fone_comercial02 = models.IntegerField(db_column='telefoneComercial02', blank=True, null=True)
-    # ddi_celular = models.IntegerField(db_column='Ddi_Celular', blank=True, null=True)
-    # ddd_celular = models.IntegerField(db_column='Ddd_Celular', blank=True, null=True)
-    fone_celular01 = models.IntegerField(db_column='telefoneCelular01')
-    fone_celular02 = models.IntegerField(db_column='telefoneCelular02', blank=True, null=True)
+    ddi_residencial = models.IntegerField(db_column='ddiResidencial', blank=True, null=True)
+    ddd_residencial = models.IntegerField(db_column='dddResidencial', blank=True, null=True)
+    fone_residencial = models.CharField(db_column='telefoneResidencial', max_length=30, blank=True, null=True)
+    ddi_comercial = models.IntegerField(db_column='ddiComercial', blank=True, null=True)
+    ddd_comercial = models.IntegerField(db_column='dddComercial', blank=True, null=True)
+    fone_comercial = models.CharField(db_column='telefoneComercial', max_length=30, blank=True, null=True)
+    ddi_celular = models.IntegerField(db_column='ddiCelular', blank=True, null=True)
+    ddd_celular = models.IntegerField(db_column='dddCelular', blank=True, null=True)
+    fone_celular = models.CharField(db_column='telefoneCelular', max_length=30, blank=True, null=True)
 
     class Meta:
         db_table = 'Pessoa'
@@ -77,3 +74,6 @@ class Aluno(models.Model):
 
     class Meta:
         db_table = 'Aluno'
+
+    def __str__(self):
+        return self.matricula
--- src/sisgee/estagio/views.py
+++ src/sisgee/estagio/views.py
@@ -1,5 +1,7 @@
 from io import TextIOWrapper
 import csv
+import logging
+import datetime
 
 from django.shortcuts import render
 
@@ -28,125 +30,135 @@ def create(request):
 def handle_files(request):
     # Lê o arquivo com os dados dos alunos e gera um dicionário do Python com eles.
     f = TextIOWrapper(request.FILES['file'].file, encoding=request.encoding)
-    reader = csv.DictReader(f)
+    line_reader = csv.DictReader(f, delimiter=';', restkey='RESTANTE_DA_LINHA')
+
+    mensagemDoLog = "os registros abaixo não foram inseridos no banco de dados pelos motivos listados:\n"
 
     # Lê cada linha do dicionário contendo os dados dos alunos. O que corresponde aos dados de um
     # aluno em específico.
-    for row in reader:
+    for row in line_reader:
 
-        # Procura por valores vazios na linha atual do dicionário de alunos, e
-        # altera esses valores de NULL para o valor do Python Nome.
-        for r in row.keys():
-            if row[r] == 'NULL':
-                row[r] = None
+        if registroPossuiErro(row):
+            # Insere o motivo da não inclusão do registro no banco e o próprio registro, além de pulá-lo.
+            mensagemDoLog += registroPossuiErro(row)
+            continue
 
-        # Altera a formatação da data de nascimento do aluno em questão.
-        data_nascimento_aluno = is_none(row['DT_NASCIMENTO'])
-        if data_nascimento_aluno:
-
-            # Altera o formato da data para ficar igual ao usado pelo banco de dados.
-            try:
-                data_nascimento_aluno = data_nascimento_aluno.split("/")
-                data_nascimento_aluno = data_nascimento_aluno[2] + "-" + data_nascimento_aluno[1] + "-" + \
-                                        data_nascimento_aluno[0]
-
-            # Caso não consiga por não ser um dado válido, apague-o com o valor None e emita uma mensagem de erro.
-            except Exception as e:
-                data_nascimento_aluno = None
-                print("Erro ao parsear a Data de Nascimento do cpf: " + is_none(row['CPF']))
-                print(e)
+            # Altera a formatação da data de nascimento do aluno em questão. O SQL só aceita no formato ano-mês-dia.
+        data_nascimento_aluno = changeDateFormat(row['CPF'], row['DT_NASCIMENTO'])
 
         # Verifica se já existe no banco os dados pessoais de determinado aluno com o cpf indicado e guarda o id
         # desse aluno. Caso não exista, o id recebe o valor None.
         try:
-            objPessoa = Pessoa.objects.get(cpf=is_none(row['CPF']))
-            id = objPessoa.pk
-        except:
-            id = None
-
-        # Cria um objeto (modelo/registro) Pessoa (que conteria os dados pessoais do aluno).
-        obj = Pessoa(cpf=is_none(row['CPF']),
-                     nome=is_none(row['NOME_ALUNO']),
-                     dt_nasc=data_nascimento_aluno,
-                     tipo_endereco=is_none(row['TIPO_LOGRADOURO']),
-                     endereco=is_none(row['LOGRADOURO']),
-                     numero=is_none(row['NUMERO']),
-                     complemento=is_none(row['COMPLEMENTO']),
-                     bairro=is_none(row['BAIRRO']),
-                     cep=is_none(row['CEP']),
-                     # distrito=is_none(row['DISTRITO']),
-                     cidade=is_none(row['MUNICIPIO']),
-                     estado=is_none(row['UF']),
-                     pais=is_none(row['PAIS']),
-                     email=is_none(row['E_MAIL']),
-                     # ddi_residencial=is_none(row['DDI_RESIDENCIAL']),
-                     # ddd_residencial=is_none(row['DDD_RESIDENCIAL']),
-                     fone_residencial01=is_none(row['FONE_RESIDENCIAL']),
-                     fone_residencial02=None,
-                     # ddi_comercial=is_none(row['DDI_COMERCIAL']),
-                     # ddd_comercial=is_none(row['DDD_COMERCIAL']),
-                     fone_comercial01=is_none(row['FONE_COMERCIAL']),
-                     fone_comercial02=None,
-                     # ddi_celular=is_none(row['DDI_CELULAR']),
-                     # ddd_celular=is_none(row['DDD_CELULAR']),
-                     fone_celular01=is_none(row['FONE_CELULAR']),
-                     fone_celular02=None)
-
-        # Se o valor de id não for None (os dados pessoais do aluno já existem no banco de dados), acrescenta o id no
-        # objeto e atualiza-o no banco de dados. Exibe um erro se não conseguir e pula esse registro do
-        # arquivo carregado.
-        if id:
-            obj.id = id
-        try:
-            obj.save()
-        except Exception as err:
-            print(err)
+            objPessoa, pessoaWasCreated = Pessoa.objects.update_or_create(
+                cpf=row['CPF'],
+                defaults={'nome': row['NOME_ALUNO'],
+                          'dt_nasc': data_nascimento_aluno,
+                          'tipo_endereco': row['TIPO_LOGRADOURO'],
+                          'endereco': row['LOGRADOURO'],
+                          'numero': row['NUMERO'],
+                          'complemento': row['COMPLEMENTO'],
+                          'bairro': row['BAIRRO'],
+                          'cep': row['CEP'],
+                          'distrito': row['DISTRITO'],
+                          'cidade': row['MUNICIPIO'],
+                          'estado': row['UF'],
+                          'pais': row['PAIS'],
+                          'email': row['E_MAIL'],
+                          'ddi_residencial': not_changed_to_none(row['DDI_RESIDENCIAL']),
+                          'ddd_residencial': not_changed_to_none(row['DDD_RESIDENCIAL']),
+                          'fone_residencial': row['FONE_RESIDENCIAL'],
+                          'ddi_comercial': not_changed_to_none(row['DDI_COMERCIAL']),
+                          'ddd_comercial': not_changed_to_none(row['DDD_COMERCIAL']),
+                          'fone_comercial': row['FONE_COMERCIAL'],
+                          'ddi_celular': not_changed_to_none(row['DDI_CELULAR']),
+                          'ddd_celular': not_changed_to_none(row['DDD_CELULAR']),
+                          'fone_celular': row['FONE_CELULAR']},
+            )
+        except Exception as e:
+            print(e)
             continue
 
+        # Verifica se a unidade (ou campus) existe no banco de dados, e cria-a (o objeto) e salva-a se não existir.
+        objCampus, campusWasCreated = Campus.objects.get_or_create(
+            nome_do_campus=row['UNIDADE'],
+        )
+
         # Verifica se o curso existe no banco de dados, e cria-o se não existir.
+        objCurso, cursoWasCreated = Curso.objects.get_or_create(
+            cod_curso=row['COD_CURSO'],
+            defaults={'nome_curso': row['CURSO'],
+                      'id_campus': objCampus},
+        )
+
         try:
-            objCurso = Curso.objects.get(cod_curso=is_none(row['COD_CURSO']))
-        except:
-            objCurso = Curso.objects.create(cod_curso=is_none(row['COD_CURSO']),
-                                            nome_curso=is_none(row['CURSO']))
+            # Verifica se a matrícula (o aluno) já existe no banco de dados através do uso da chave primária, e
+            # guarda-o se existir. Caso não exista, configura o objeto para None e não salva no banco de dado
+            # essas informações.
+            objAluno, alunoWasCreated = Aluno.objects.get_or_create(
+                id_aluno=objPessoa,
+                id_curso=objCurso,
+                defaults={'matricula': row['MATRICULA'],
+                          'situacao': row['SITUACAO']},
+            )
+        except Exception as e3:
+            print(e3)
+            continue
 
-        # Verifica se a unidade (ou campus) existe no banco de dados, e cria-a e salva-a se não existir.
-        try:
-            objCampus = Campus.objects.get(nome_do_campus=is_none(row['UNIDADE']))
-        except:
-            objCampus = Campus.objects.create(nome_do_campus=is_none(row['UNIDADE']))
+    # Os registros não inclusos no banco de dados são gravados num arquivo de log, caso exista algum.
+    if len(mensagemDoLog) > 82:
+        logging.getLogger("bd log").warning(mensagemDoLog)
 
-            # Salva os dados do campus.
-            try:
-                objCampus.save()
-            except Exception as err:
-                print(err)
-
-        # Verifica se a matrícula (o aluno) já existe no banco de dados através do uso da chave primária, e
-        # guarda-o se existir. Caso não exista, configura o objeto para None e não salva no banco de dado
-        # essas informações.
-        try:
-            objAluno = Aluno.objects.filter(id_aluno__pk=obj.pk)
-        except:
-            objAluno = None
-
-        # Existindo algum aluno (ou vários alunos) com determinada matrícula, separa a chave primária de cada curso
-        # numa lista, e ??? Se tiver a chave do curso encontrado no registro atual do aluno, pula esse registro.
-        if objAluno:
-            lis = list()
-            for valueobj in objAluno:
-                lis.append(valueobj.nome_curso.pk)
-            if objCurso.pk in lis:
-                continue
-
-        # Cria o objeto aluno e salva-o no banco de dados.
-        obj_ps = Aluno(matricula=is_none(row['MATRICULA']), situacao=is_none(row['SITUACAO']), id_curso=objCurso,
-                       id_aluno=obj)
-        obj_ps.save()
+    # Dando tudo certo, retorna o valor True.
     return True
 
 
-def is_none(value):
+def registroPossuiErro(linha, mensagem_de_log='', separador='\t'):
+    motivo_do_log = "Registro "
+    registro_para_log = ""
+
+    if linha['CPF'] == '':
+        motivo_do_log += "sem CPF"
+
+    if ('/' in linha['DT_NASCIMENTO']) and (int(linha['DT_NASCIMENTO'][-4:]) <= datetime.datetime.now().year - 90)\
+            and len(motivo_do_log) == 9:
+        motivo_do_log += "com idade maior que 90 anos"
+    elif ('/' in linha['DT_NASCIMENTO']) and (int(linha['DT_NASCIMENTO'][-4:]) <= datetime.datetime.now().year - 90)\
+            and len(motivo_do_log) > 9:
+        motivo_do_log += ", com idade maior que 90 anos"
+
+    if (not '/' in linha['DT_NASCIMENTO']) and len(motivo_do_log) == 9:
+        motivo_do_log += "com dados deslocados para direita"
+    elif (not '/' in linha['DT_NASCIMENTO']) and len(motivo_do_log) > 9:
+        motivo_do_log += ", com dados deslocados para direita"
+
+    if len(motivo_do_log) > 9:
+
+        for chave, valor in linha.items():
+            registro_para_log += "{0} = {1}{2}".format(chave, valor, separador)
+
+        mensagem_de_log = "{:42}\t{}\n".format(motivo_do_log, registro_para_log)
+
+    return mensagem_de_log
+
+
+def changeDateFormat(cpf, date):
+    # Formata a data apenas se ela não for vazia.
+    if date != '':
+
+        # Altera o formato da data para ficar igual ao usado pelo banco de dados.
+        try:
+            date = date.split("/")
+            date = date[2] + "-" + date[1] + "-" + date[0]
+
+        # Caso não consiga por não ser um dado válido, apague-o com o valor None e emita uma mensagem de erro.
+        except Exception as e:
+            date = ''
+            print("Erro ao formatar a Data de Nascimento do cpf: " + cpf + "\n\nEsse campo não possui uma data válida!")
+
+    return date
+
+
+def not_changed_to_none(value):
     if len(value) == 0:
         return None
     return value
